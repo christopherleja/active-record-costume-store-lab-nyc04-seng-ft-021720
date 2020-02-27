d3d970df2fd72e257580092fadad98f67b23a3f4
Christopher Leja <christopher.leja@gmail.com>
2020-02-27 21:35:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_store do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :employee_count
+      t.boolean :is_open
+      t.float :open_time
+      t.float :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_house do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.price :float
+      t.boolean :is_family_friendly?
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+  end
+end
