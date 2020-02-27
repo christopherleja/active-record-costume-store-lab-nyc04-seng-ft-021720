a56282e7bb54526cb7c382e797e57b09ad2fd666
Christopher Leja <christopher.leja@gmail.com>
2020-02-27 22:13:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :costume_inventory
-      t.integer :employees_count
+      t.integer :num_of_employees
       t.boolean :still_in_business
       t.float :open_time
       t.float :closing_time
